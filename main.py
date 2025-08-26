6b4511a72566bfafb00cec1abf2e043b2f215cd6
NicolasPllr1 <nicolas.pllr1@gmail.com>
2025-08-26 20:41:05 +0200

    add token class

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class TokenKind(Enum):
+    PLUS = "+"
+    MINUS = "-"
+    SLASH = "/"
+    #
+    QUOTE = "quote"
+    QUOTE_ABR = "'"
+    CONS = "cons"
+    #
+    NIL = "nil"
+    TRUE = "t"
+    #
+    STRING = None
+    #
+    LEFT_PAREN = "("
+    RIGHT_PAREN = ")"
+
+
+@dataclass
+class Token:
+    kind: TokenKind
+    lexeme: str | None  # TODO: think about the need for both lexeme and literal
+    literal: str | None
+
+
 def main():
     print("Hello from lisp-interpreter!")
 
